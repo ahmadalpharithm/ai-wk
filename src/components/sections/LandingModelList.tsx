257f3c25379203349e3796039b764411ae1c3090
Engraya <ahmadyakubuaya@gmail.com>
2025-03-21 10:20:33 +0100

    Fixed

--- src/components/sections/LandingModelList.tsx
+++ src/components/sections/LandingModelList.tsx
@@ -14,22 +14,27 @@ interface LandingModelListProps {
 
 const LandingModelList: React.FC<LandingModelListProps> = ({ modelData, activeButton }) => {
   const swiperRef = useRef<any>(null);
-  const buttons = Object.keys(modelData); 
+  const buttons = Object.keys(modelData);
 
   useEffect(() => {
-    const activeIndex = buttons.indexOf(activeButton); 
+    const activeIndex = buttons.indexOf(activeButton);
     if (swiperRef.current && activeIndex !== -1) {
       swiperRef.current.swiper.slideTo(activeIndex);
     }
   }, [activeButton, buttons]);
 
   return (
-    <div className="max-w-full flex justify-center items-center mx-auto">
+    <div className="max-w-full mx-auto">
       <Swiper
         ref={swiperRef}
         modules={[Pagination, EffectCoverflow]}
-        spaceBetween={30}
-        slidesPerView={1.7}
+        breakpoints={{
+          320: { slidesPerView: 1.2, spaceBetween: 10 }, // Mobile
+          768: { slidesPerView: 1.4, spaceBetween: 20 }, // Tablet
+          1024: { slidesPerView: 1.7, spaceBetween: 30 }, // Small screens
+          1440: { slidesPerView: 2, spaceBetween: 40 }, // Larger screens
+          2560: { slidesPerView: 2.5, spaceBetween: 30 }, // 4K screens
+        }}
         centeredSlides={true}
         grabCursor={true}
         effect="coverflow"
@@ -40,7 +45,7 @@ const LandingModelList: React.FC<LandingModelListProps> = ({ modelData, activeBu
           modifier: 0.5,
           slideShadows: false,
         }}
-        loop={false} // Disable loop to maintain correct order
+        loop={false}
         className="mySwiper"
       >
         {buttons.map((button) => (
@@ -48,9 +53,7 @@ const LandingModelList: React.FC<LandingModelListProps> = ({ modelData, activeBu
             {({ isActive }) => (
               <div
                 className={`transition-all duration-500 ease-in-out ${
-                  isActive
-                    ? "transform translate-y-[-18px] scale-105"
-                    : "transform translate-y-20"
+                  isActive ? "transform translate-y-[-18px] scale-105" : "transform translate-y-20"
                 }`}
               >
                 <LandingModelCard
